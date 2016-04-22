c9189072a1956c8e19a16d96bee3baf8d153262b
juner <junliu.work@gmail.com>
2016-04-23 00:14:18 +0800

    haha

--- src/helloflask.py
+++ src/helloflask.py
@@ -7,7 +7,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.pagedown import PageDown
 from flask.ext.pagedown.fields import PageDownField
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 from flask import Flask, render_template, session, redirect, url_for
 from datetime import datetime
@@ -30,11 +30,6 @@ manager = Manager(app)
 pagedown = PageDown(app)
 
 
-class NameForm(Form):
-    name = StringField("what is your name?", validators=[InputRequired()])
-    submit = SubmitField('submit')
-
-
 class NewBlogForm(Form):
     title = StringField('标题', validators=[InputRequired()])
     body = PageDownField('写点儿什么', validators=[InputRequired()])
@@ -47,13 +42,19 @@ class EditBlogForm(Form):
     submit = SubmitField('保存修改')
 
 
+class ContentForm(Form):
+    body = PageDownField('添加点笔记？', validators=[InputRequired()])
+    submit = SubmitField('添加新内容')
+
+
 class Blog(db.Model):
-    __talbename__ = 'blogs'
+    __tablename__ = 'blogs'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), unique=True)
     body = db.Column(db.Text)
     body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    contents = db.relationship('BlogContent', backref='blog')
 
     def __repr__(self):
         return '<Blog %s>' % self.title
@@ -67,7 +68,22 @@ class Blog(db.Model):
         target.body_html = bleach.linkify(cleaned_body_html)
 
 
+class BlogContent(db.Model):
+    __tablename__ = 'contents'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
+
+
+
+    def __repr__(self):
+        return '<Belong to %s>' % self.blog.title
+
+
 db.event.listen(Blog.body, 'set', Blog.on_changed_body)
+db.event.listen(BlogContent.body, 'set', Blog.on_changed_body)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -83,20 +99,24 @@ def new_blog():
     if blogform.validate_on_submit():
         blog = Blog(title=blogform.title.data,
                     body=blogform.body.data,
-                    timestamp = datetime.utcnow()
+                    timestamp=datetime.utcnow()
                     )
         db.session.add(blog)
-
-        session['name'] = blogform.title.data
-        session['text'] = blogform.body.data
         return redirect(url_for('index'))
     return render_template('newblog.html', blogform=blogform, current_time=datetime.utcnow())
 
 
-@app.route('/blogs/<int:id>')
+@app.route('/blogs/<int:id>', methods=['GET', 'POST'])
 def blog(id):
+    content_form = ContentForm()
     blog = Blog.query.get_or_404(id)
-    return render_template('blog.html', blog=blog)
+    if content_form.validate_on_submit():
+        new_content = BlogContent(body=content_form.body.data,
+                                  blog=blog,
+                                  timestamp=datetime.utcnow())
+        db.session.add(new_content)
+        return redirect(url_for('blog', id=blog.id))
+    return render_template('blog.html', blog=blog, content_form=content_form)
 
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
